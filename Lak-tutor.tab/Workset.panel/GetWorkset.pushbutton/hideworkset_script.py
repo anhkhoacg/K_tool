507a039ba2a9a4e3614736d0ad880a3245acb786
anhkhoacg <anhkhoacg@gmail.com>
2024-04-17 22:05:00 +0700

    ok

--- Lak-tutor.tab/Workset.panel/GetWorkset.pushbutton/hideworkset_script.py
+++ Lak-tutor.tab/Workset.panel/GetWorkset.pushbutton/hideworkset_script.py
@@ -2,16 +2,19 @@ __title__ = " hide Workset"
 __author__ = " Khoa Le"
 __doc__ = " set workset to hide"
 
+
 # Import the necessary modules
-import clr
-clr.AddReference('RevitAPI')
-clr.AddReference('RevitAPIUI')
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
+import clr
+
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
 
 # Get the active document and the user interface
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
 
 # Define a function to hide the workset of the selected elements
 def hide_workset(selection):
@@ -24,16 +27,17 @@ def hide_workset(selection):
         # Get the element and its workset id
         element = doc.GetElement(eid)
         workset_id = element.WorksetId
-        
+
         # Get the workset visibility settings for the active view
         workset_visibility = doc.ActiveView.GetWorksetVisibility(workset_id)
-        
+
         # If the workset is visible, hide it
-        #if workset_visibility == WorksetVisibility.Visible:
+        # if workset_visibility == WorksetVisibility.Visible:
         doc.ActiveView.SetWorksetVisibility(workset_id, WorksetVisibility.Hidden)
-            
+
     # Commit the transaction
     t.Commit()
+
 
 # Get the selected elements
 selection = uidoc.Selection.GetElementIds()
