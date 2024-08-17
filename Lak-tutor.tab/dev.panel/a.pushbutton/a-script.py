0df46569113d68fb4ee52b105ad4bb2edbd28272
anhkhoacg <anhkhoacg@gmail.com>
2024-08-17 18:14:24 +0700

    20240817

--- Lak-tutor.tab/dev.panel/a.pushbutton/a-script.py
+++ Lak-tutor.tab/dev.panel/a.pushbutton/a-script.py
@@ -1,28 +1,10 @@
-import clr
-clr.AddReference("RevitAPI")
-from Autodesk.Revit.DB import *
-
-# Get the active view
-doc = __revit__.ActiveUIDocument.Document
-activeView = doc.ActiveView
-
-# Create a FilteredElementCollector for the active view
-collector = FilteredElementCollector(doc, activeView.Id)
-
-# Get all elements in the active view
-elements = collector.ToElements()
-
-# Initialize an empty set to store unique category names
-unique_categories = set()
-
-# Iterate over each element and get its category
-for element in elements:
-    type_id = element.GetTypeId()
-    if type_id != ElementId.InvalidElementId:
-        category = doc.GetElement(type_id).Category
-        if category is not None:
-            unique_categories.add(category.Name)
-
-# Print the unique category names
-#for cat_name in unique_categories:
-print(unique_categories)
+from pyrevit import forms
+layout = '<Window ' \
+         'xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" ' \
+         'xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" ' \
+         'ShowInTaskbar="False" ResizeMode="NoResize" ' \
+         'WindowStartupLocation="CenterScreen" ' \
+         'HorizontalContentAlignment="Center">' \
+         '</Window>'
+w = forms.WPFWindow(layout, literal_string=True)
+w.show()
