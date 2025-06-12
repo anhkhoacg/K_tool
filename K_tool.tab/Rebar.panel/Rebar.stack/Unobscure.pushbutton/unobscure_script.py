70ebb363d81f33e7f20b9e74f90c6d25450d5f06
anhkhoacg <anhkhoacg@gmail.com>
2025-06-12 11:59:20 +0700

    Updates show all rebar tool

--- K_tool.tab/Rebar.panel/Rebar.stack/Unobscure.pushbutton/unobscure_script.py
+++ K_tool.tab/Rebar.panel/Rebar.stack/Unobscure.pushbutton/unobscure_script.py
@@ -1,37 +1,33 @@
-__doc__ = "Make all bars unobscured in view."
-__title__ = "Unobscure Bars"
-__author__ = "MWolinski"
-import Autodesk
-from Autodesk.Revit import DB
-from Autodesk.Revit.UI import *
-from Autodesk.Revit.DB import *
-"from rebar_selector import RebarSelector"
+__doc__ = "Make all bars obscured in view."
+__title__ = "Obscure Bars"
+__author__ = "Wolinski"
 
-'''
+
+
+
+
+
+
+
+
+
+import clr
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import *
+from Autodesk.Revit.UI import TaskDialog
+
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 view = doc.ActiveView
 
-rs = RebarSelector(doc, uidoc)
-rebar_collector = rs.get_all_rebars()
-'''
-
-
-# Get the current document
-doc = DocumentManager.Instance.CurrentDBDocument
-
-# Get the active view
-active_view = doc.ActiveView
-
-collector = FilteredElementCollector(doc, active_view.Id)
-rebar_elements = collector.OfCategory(BuiltInCategory.OST_Rebar).ToElements()
-
-
+# Collect rebar elements visible in current view
+rebar_collector = FilteredElementCollector(doc, view.Id).OfClass(Rebar)
 
 x = True
-
-t = DB.Transaction(doc, "Unobscured")
+t = Transaction(doc, "ObscuredBars")
 t.Start()
-for rebar in rebar_elements:
+for rebar in rebar_collector:
     rebar.SetUnobscuredInView(view, x)
 t.Commit()
